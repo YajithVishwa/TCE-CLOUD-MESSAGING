772b0bdb5977ebaa5e6d307062d8d9f8c6c65e6e
YajithVishwa <yajith@student.tce.edu>
2020-07-25 21:51:09 +0530

    app

--- app/src/main/java/com/yajith/messaging/FirstTime/Swipe/SwipeActivity.java
+++ app/src/main/java/com/yajith/messaging/FirstTime/Swipe/SwipeActivity.java
@@ -5,6 +5,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
 import com.yajith.messaging.R;
 
 public class SwipeActivity extends AppCompatActivity {
@@ -15,8 +16,10 @@ public class SwipeActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         setContentView(R.layout.activity_swipe);
         ViewPager viewPager=findViewById(R.id.page);
+        TabLayout tabLayout=findViewById(R.id.tab);
         FragmentPageAdapter fragmentPageAdapter=new FragmentPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(fragmentPageAdapter);
+        tabLayout.setupWithViewPager(viewPager,true);
     }
 
     @Override
